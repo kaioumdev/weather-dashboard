9e7167a5a4c3cce9462672d64911c0b55bbbaef3
kaioumdev <afrankaiyum34@gmail.com>
2026-03-12 18:46:40 +0600

    create custom hook

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+const useWeather = () => {
+    const [weatherData, setWeatherData] = useState({
+        location: "",
+        climate: "",
+        temperature: "",
+        maxTemperature: "",
+        minTemperature: "",
+        humidity: "",
+        cloudPercentage: "",
+        wind: "",
+        time: "",
+        longitude: "",
+        latitude: ""
+    });
+
+    const [loading, setLoading] = useState({
+        state: false,
+        message: ""
+    });
+
+    const [error, setError] = useState(null)
+};
+
+export default useWeather;
