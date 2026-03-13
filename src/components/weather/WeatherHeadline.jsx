4637456cd517fadd4b38db1ede71bfb007304ef0
kaioumdev <afrankaiyum34@gmail.com>
2026-03-14 02:52:58 +0600

    feat: make a feature to get date time

--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -1,23 +1,27 @@
-import Cloud from '../../assets/cloud.svg'
-import Pin from '../../assets/pin.svg'
+import { useContext } from "react";
+import Cloud from "../../assets/cloud.svg";
+import Pin from "../../assets/pin.svg";
+import { WeatherContext } from "../../context";
 const WeatherHeadline = () => {
-    return (
-          <div>
-            <div className="max-md:flex items-center justify-between md:-mt-10">
-              <img src={Cloud} alt="cloud" />
-              <div className="max-md:flex items-center max-md:space-x-4">
-                <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-                  16°
-                </h1>
-                <div className="flex items-center space-x-4 md:mb-4">
-                  <img src={Pin} />
-                  <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
-                </div>
-              </div>
-            </div>
-            <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+    const {loading, weatherData} = useContext(WeatherContext);
+    const {climate, location, temperature, time} = weatherData;
+  return (
+    <div>
+      <div className="max-md:flex items-center justify-between md:-mt-10">
+        <img src={Cloud} alt="cloud" />
+        <div className="max-md:flex items-center max-md:space-x-4">
+          <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
+            {Math.round(temperature)}
+          </h1>
+          <div className="flex items-center space-x-4 md:mb-4">
+            <img src={Pin} />
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
-    )
+        </div>
+      </div>
+      <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+    </div>
+  );
 };
 
-export default WeatherHeadline
+export default WeatherHeadline;
