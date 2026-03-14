b3f7a7ae408e4737fd0b6f0a3cdbd2df011ffca8
kaioumdev <afrankaiyum34@gmail.com>
2026-03-14 22:28:09 +0600

    feat(weatherIcon): successfully show the weather icon

--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -3,13 +3,42 @@ import Cloud from "../../assets/cloud.svg";
 import Pin from "../../assets/pin.svg";
 import { WeatherContext } from "../../context";
 import { getFormattedDate } from "../../utils/date-util";
+import CloudIcon from "../../assets/cloud.svg";
+import HazeIcon from "../../assets/haze.svg";
+import SnowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
+import RainIcon from "../../assets/rainy.svg";
+import ThunderIcon from "../../assets/thunder.svg";
 const WeatherHeadline = () => {
-    const {loading, weatherData} = useContext(WeatherContext);
-    const {climate, location, temperature, time} = weatherData;
+  const { loading, weatherData } = useContext(WeatherContext);
+  const { climate, location, temperature, time } = weatherData;
+
+  function getWeatherIcon(climate){
+    switch(climate){
+      case 'Rain':
+        return RainIcon;
+      case 'Clouds':
+        return CloudIcon;
+      case 'Clear':
+        return SunnyIcon;
+      case 'Snow':
+        return SnowIcon;
+      case 'Thunder':
+        return ThunderIcon;
+      case 'Fog':
+        return HazeIcon;
+      case 'Haze':
+        return HazeIcon;
+      case 'Mist':
+        return HazeIcon;
+      default: 
+        return SunnyIcon
+    }
+  }
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src={Cloud} alt="cloud" />
+        <img src={getWeatherIcon(climate)} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
             {Math.round(temperature)}
@@ -20,7 +49,10 @@ const WeatherHeadline = () => {
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">{getFormattedDate(time, "time", false)} - {getFormattedDate(time, "date", false)}</p>
+      <p className="text-sm lg:text-lg">
+        {getFormattedDate(time, "time", false)} -{" "}
+        {getFormattedDate(time, "date", false)}
+      </p>
     </div>
   );
 };
