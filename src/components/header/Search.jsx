e279cf3ed1dc864b159f0aa097c251d626e744ff
kaioumdev <afrankaiyum34@gmail.com>
2026-03-25 23:05:59 +0600

    successfully implements debounce to the project

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -2,23 +2,31 @@ import { useContext, useState } from 'react';
 import SearchLogo from '../../assets/search.svg'
 import { LocationContext } from '../../context';
 import { getLocationByName } from '../../data/location-data';
+import { useDebounce } from '../../hooks';
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const {setSelectedLocation} = useContext(LocationContext)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(searchTerm);
-    const fetchedLocation = getLocationByName(searchTerm);
+    // console.log(searchTerm);
+    // const fetchedLocation = getLocationByName(searchTerm);
+    // console.log(fetchedLocation);
+    // setSelectedLocation({...fetchedLocation})
+  };
+
+  const doSearch = useDebounce((term) => {
+    console.log(term);
+    const fetchedLocation = getLocationByName(term);
     console.log(fetchedLocation);
     setSelectedLocation({...fetchedLocation})
-  };
+  }, 500)
 
   const handleChange = (e) => {
     const value = e.target.value;
     setSearchTerm(value);
-    
-  }
+    doSearch(value)
+  };
   return (
     <form action="#" onSubmit={handleSubmit}>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
