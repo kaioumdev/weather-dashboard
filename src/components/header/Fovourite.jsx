c63f8758fee88e82665f104503037773166e597e
kaioumdev <afrankaiyum34@gmail.com>
2026-03-25 19:13:19 +0600

    feat(modal): show modal by condition successfully

--- src/components/header/Fovourite.jsx
+++ src/components/header/Fovourite.jsx
@@ -1,9 +1,9 @@
 import Heart from '../../assets/heart.svg'
-const Fovourite = () => {
+const Fovourite = ({onShow}) => {
     return (
          <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
             <img src={Heart} alt="" />
-            <span>Favourite Locations</span>
+            <span onClick={onShow}>Favourite Locations</span>
           </div>
     )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import FavouriteListModal from "./FavouriteListModal";
 import Fovourite from "./Fovourite";
 import Logo from "./Logo";
 import Search from "./Search";
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,9 +13,9 @@ const Header = () => {
 
         <div className="flex items-center gap-4 relative">
           <Search/>
-          <Fovourite/>
+          <Fovourite onShow={() => setShowModal(!showModal)}/>
 
-          <FavouriteListModal/>
+          {showModal && <FavouriteListModal/>}
         </div>
       </nav>
     </header>
