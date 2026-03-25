12508eb9150ad96c2e9013c1bd222e3573fc2e27
kaioumdev <afrankaiyum34@gmail.com>
2026-03-25 21:38:14 +0600

    feat(bg-img): successfully set the bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import Page from "./Page";
 import {
   FavouriteProvider,
   LocationProvider,
@@ -11,14 +12,7 @@ function App() {
     <LocationProvider>
       <WeatherProvider>
         <FavouriteProvider>
-          <div className="grid place-items-center h-screen">
-            <Header />
-            <main>
-              <section>
-                <WeatherBoard />
-              </section>
-            </main>
-          </div>
+          <Page></Page>
         </FavouriteProvider>
       </WeatherProvider>
     </LocationProvider>
